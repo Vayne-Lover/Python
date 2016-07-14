508af741278782be98abb5b195c2003fc4641e07
Vayne-Lover <406378362@qq.com>
2016-07-14 13:52:01 +0800

    Add daysbetweendays to calculate the days between two input days. In order to finish the function i divide days into three parts,the day before the end year of start year,the year from start year+1 to end year and the days of the end year.

--- Days old/main.py
+++ Days old/main.py
@@ -6,6 +6,7 @@ def isleapyear(n):
             return True
     return False
 #print isleapyear(2004)
+
 def daysinyear(y,m,d):
     daysOfMonths=[ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     result=0
@@ -17,4 +18,25 @@ def daysinyear(y,m,d):
     if isleapyear(y):
         if m>2:
             result=result+1
+    return result
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    result=0
+    if year1==year2 :
+        result=daysinyear(year2,month2,day2)-daysinyear(year1,month1,day1)
+    else:
+        if isleapyear(year1):
+            temp1=366-daysinyear(year1,month1,day1)
+        else:
+            temp1=365-daysinyear(year1,month1,day1)
+        temp2=daysinyear(year2,month2,day2)
+        temp=0
+        yeartemp=year1+1
+        while yeartemp<year2:
+            if isleapyear(yeartemp):
+                temp=temp+366
+            else:
+                temp=temp+365
+            yeartemp=yeartemp+1
+        result=temp+temp1+temp2
     return result
