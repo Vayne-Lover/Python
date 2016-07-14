c592b85f5b366dd6422dc503fc4cda41a77ffdea
Vayne-Lover <406378362@qq.com>
2016-07-14 13:56:28 +0800

    Add test

--- Days old/main.py
+++ Days old/main.py
@@ -40,3 +40,18 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
             yeartemp=yeartemp+1
         result=temp+temp1+temp2
     return result
+
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58),
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+                  for (args, answer) in test_cases:
+                      result = daysBetweenDates(*args)
+                          if result != answer:
+                              print "Test with data:", args, result,answer
+                                  else:
+                                      print "Test case passed!"
+
+test()
