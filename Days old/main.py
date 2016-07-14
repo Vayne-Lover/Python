c3c9214c44a6f1b904026bcdc937cc6e36ff526d
Vayne-Lover <406378362@qq.com>
2016-07-14 13:50:26 +0800

    Add daysinyear to calculate the number of the day in a year

--- Days old/main.py
+++ Days old/main.py
@@ -6,3 +6,15 @@ def isleapyear(n):
             return True
     return False
 #print isleapyear(2004)
+def daysinyear(y,m,d):
+    daysOfMonths=[ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    result=0
+    temp=1
+    while temp<m :
+        result=result+daysOfMonths[temp-1]
+        temp=temp+1
+    result=result+d
+    if isleapyear(y):
+        if m>2:
+            result=result+1
+    return result
