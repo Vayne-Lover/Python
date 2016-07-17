abce42752b3db219fad71d83b2c1ceef46a396a1
Vayne-Lover <406378362@qq.com>
2016-07-17 12:48:30 +0800

    Add union and get_all_links

--- WebCrawler/main.py
+++ WebCrawler/main.py
@@ -6,3 +6,20 @@ def get_next_target(page):
     end_quote = page.find('"', start_quote + 1)
     url = page[start_quote + 1:end_quote]
     return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
