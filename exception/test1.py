98771f6a7a96ccedf56e3619433e2451d5c3251f
Vayne-Lover <406378362@qq.com>
2016-12-24 12:38:31 +0800

    Use finally and so on.

--- exception/test1.py
+++ exception/test1.py
@@ -1,16 +1,56 @@
 #!/usr/local/bin/python
-class MuffledCalculator:
-  muffled=False
-  def calc(self,expr):
-    try:
-      return eval(expr)
-    except ZeroDivisionError:
-      if self.muffled:
-        print "Can't divide zero"
-      else:
-        raise
-a=MuffledCalculator()
-print a.calc('2/1')
+#class MuffledCalculator:
+#  muffled=False
+#  def calc(self,expr):
+#    try:
+#      return eval(expr)
+#    except (ZeroDivisionError,TypeError):
+#      if self.muffled:
+#        print "There are errors."
+#      else:
+#        raise
+#a=MuffledCalculator()
+#print a.calc('2/1')
+##print a.calc('2/"dsf"')
+##print a.calc('1/0')
+#a.muffled=True
 #print a.calc('1/0')
-a.muffled=True
-print a.calc('1/0')
+#class Test:
+#  def init(self): 
+#    try:
+#      x=1
+#      y='sg'
+#      print x/y
+#    except (ZeroDivisionError,TypeError),e:
+#      print e
+#a=Test()
+#a.init()    
+#class Test1:
+#  def init(self): 
+#    try:
+#      x=1
+#      y='sg'
+#      print x/y
+#    except Exception,e:
+#      print e
+#a=Test1()
+#a.init()
+#try:
+#  print 'Go!'
+#except Exception,e:
+#  print e
+#else:
+#  print 'Planned.'
+x=1
+try:
+  x=2
+  print x
+  x=1/0
+except Exception,e:
+  x=3
+  print x
+  print e
+finally:
+  x=4
+  print x
+
