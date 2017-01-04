905f37df72402e793eff852175e10f6590e2ad0e
Vayne-Lover <406378362@qq.com>
2017-01-04 22:29:05 +0800

    Add select.

---
+++ network/select.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+import socket
+import select
+
+s=socket.socket()
+
+host=socket.gethostname()
+port=1234
+s.bind((host,port))
+
+s.listen(5)
+inputs=[s]
+
+while True:
+  rs,ws,es=select.select(inputs,[],[])
+  for r in rs:
+    if r is s:
+      c,addr=s.accept()
+      print 'Get connection.',addr
+      inputs.append(c)
+    else:
+      try:
+        data=r.recv(1024)
+        disconnected=not data
+      except socket.error:
+        disconnected=True
+
+      if disconnected:
+        print r.getpeername(),'disconnected'
+        inputs.remove(r)
+      else:
+        print data
