6a27ee93471ce7c3c9058c2143f2cb82adf4ef31
Vayne-Lover <406378362@qq.com>
2017-01-07 14:48:25 +0800

    Add unittest.

---
+++ test/test.py
@@ -0,0 +1,20 @@
+#!/usr/loacl/bin/python
+import unittest,my_math
+
+class ProductTestCase(unittest,TestCase):
+  def testInt(self):
+    for x in xrange(-10,10):
+      for y in xrange(-10,10):
+        p=my_math.product(x,y)
+        self.failUnless(p==x*y,'Int failed.')
+
+  def testFloat(self):
+    for x in xrange(-10,10):
+      for y in xrange(-10,10):
+        x=x/10.0
+        y=y/10.0
+        p=my_math.product(x,y)
+        self.failUnless(p==x*y,'Float failed.')
+
+if __name__=='__main__':
+  unittest.main()
